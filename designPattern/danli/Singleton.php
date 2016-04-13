e945e0cbe0234395f646b6457ba8a9880c02c925
guojiezhu <984837558@qq.com>
2016-04-13 11:23:09 +0800

    增加两个方法

--- designPattern/danli/Singleton.php
+++ designPattern/danli/Singleton.php
@@ -49,4 +49,6 @@ class Singleton
         }
         return  self::$_instance;
     }
+
+
 }
--- designPattern/factory/Factory.php
+++ designPattern/factory/Factory.php
@@ -1,10 +1,10 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: guojiezhu
  * Date: 16/4/12下午5:40
  */
-
 abstract class FactoryMethod
 {
     const CHEAP = 1;
@@ -15,10 +15,87 @@ abstract class FactoryMethod
     public function create($type)
     {
         $obj = $this->createVehicle($type);
-        $obj -> setColor('#f00');
+        $obj->setColor('#f00');
         return $obj;
     }
 }
 
 
+class ItalianFactory extends FactoryMethod
+{
+    protected function createVehicle($type)
+    {
+        // TODO: Implement createVehicle() method.
+        switch ($type) {
+            case parent::CHEAP:
+                return new Bicycle();
+                break;
+            case parent::FAST:
+                return new Ferrari();
+                break;
+            default:
+                throw new \InvalidArgumentException("$type is not a valid vehicle");
+        }
+    }
+}
 
+class GermanFactory extends FactoryMethod
+{
+    protected function createVehicle($type)
+    {
+        switch ($type) {
+            case parent::CHEAP:
+                return new Bicycle();
+                break;
+            case parent::FAST:
+                $obj = new Porsche();
+                $obj->addTuningAMG();
+                return $obj;
+                break;
+            default:
+                throw new \InvalidArgumentException("$type is not a valid vehicle");
+        }
+    }
+}
+
+
+interface VehicleInterface
+{
+    public function setColor($rgb);
+}
+
+class Porsche implements VehicleInterface
+{
+    protected $color;
+
+    public function setColor($rgb)
+    {
+        // TODO: Implement setColor() method.
+    }
+
+    public function addTuningAMG()
+    {
+    }
+}
+
+class Bicycle implements VehicleInterface
+{
+    protected $color;
+
+    public function setColor($rgb)
+    {
+        // TODO: Implement setColor() method.
+        $this->color = $rgb;
+    }
+}
+
+class Ferrari implements VehicleInterface
+{
+    protected $color;
+
+    public function setColor($rgb)
+    {
+        // TODO: Implement setColor() method.
+        $this->color = $rgb;
+    }
+}
