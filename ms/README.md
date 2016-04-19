d205f130a6fc7f3f8a742755b0bd7c6806b4818c
guojiezhu <984837558@qq.com>
2016-04-19 09:59:26 +0800

    增加测试

---
+++ ms/README.md
@@ -0,0 +1,5 @@
+#遇到的面试题
+1.yx 优信二手车, 导出csv
+ 1).链接mysql
+ 2).gbk ->utf8
+ 3).header导出
---
+++ ms/suanfa.hp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guojiezhu
+ * Date: 16/4/18下午3:20
+ */
+$arr[false] = 1;$arr['']=2;$arr[null]=3;$arr[0]=4;
