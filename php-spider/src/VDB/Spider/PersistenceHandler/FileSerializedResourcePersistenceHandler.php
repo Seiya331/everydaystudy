aa37761537a38181c901974af3959634c85624a3
zhuguojie <984837558@qq.com>
2016-05-20 14:38:58 +0800

    增加

---
+++ php-spider/src/VDB/Spider/PersistenceHandler/FileSerializedResourcePersistenceHandler.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Matthijs van den Bos <matthijs@vandenbos.org>
+ * @copyright 2013 Matthijs van den Bos
+ */
+
+namespace VDB\Spider\PersistenceHandler;
+
+use Symfony\Component\Finder\Finder;
+use VDB\Spider\PersistenceHandler\FilePersistenceHandler;
+use VDB\Spider\Resource;
+
+class FileSerializedResourcePersistenceHandler extends FilePersistenceHandler implements PersistenceHandlerInterface
+{
+    public function persist(Resource $resource)
+    {
+        $fileName = urlencode($resource->getUri()->toString());
+        $file = new \SplFileObject($this->getResultPath() . $fileName, 'w');
+        $this->totalSizePersisted += $file->fwrite(serialize($resource));
+    }
+
+    /**
+     * @return Resource
+     */
+    public function current()
+    {
+        return unserialize($this->getIterator()->current()->getContents());
+    }
+}
---
+++ phpspider/phpspider/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS['config']['db'] = array(
+    'host'=>'127.0.0.1',
+    'user'=>'root',
+    'pass'=>'zhuguojei1989',
+    'name'=>'test',
+);
+
+$GLOBALS['config']['redis'] = array(
+    'host'=>'127.0.0.1',
+    'port'=>6379,
+);
+
+
